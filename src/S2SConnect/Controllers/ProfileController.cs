2ebaf7e0b3e19ed6ba99bac076b64b9dba74bff9
Alphasigmation <peter.zaika@live.com>
2015-10-10 14:02:51 -0700

    Initial Commit

---
+++ src/S2SConnect/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace S2SConnect.Models
+{
+    public class ProfileController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View("~/Views/Profile/Index");
+        }
+
+        public IActionResult FindOrgs()
+        {
+            return View("~/Views/Profile/Era");
+        }
+    }
+}
---
+++ src/S2SConnect/Services/SQL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace S2SConnect.Services
+{
+    public class SQL
+    {
+        public const string connection = "Server=tcp:a4csgx5klf.database.windows.net,1433;Database=connectDBS;User ID=DBadmin@a4csgx5klf;Password=li0Ns8rAbb1t;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+        public void LoginWrite(string username, string passwrd)
+        {
+            SqlConnection _sqlConn = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "INSERT INTO dbo.Students(username,password) VALUES (" + "\"" + username + "\"" +","+ "\"" + passwrd + "\"" + " );";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = _sqlConn;
+
+            _sqlConn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            _sqlConn.Close();
+        }
+    }
+}
